ff173542a6436c674681430df5f55d5a5917e7ab
Vinicius Trindade <viniciustrin@live.com>
2018-08-03 11:52:41 -0300

    Commit Inicial

---
+++ Adocoes/Adocoes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Adocoes.MvcApplication" Language="C#" %>
---
+++ Adocoes/Adocoes/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Adocoes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Adocoes.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Adocoes.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
